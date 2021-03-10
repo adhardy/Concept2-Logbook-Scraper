b04db65621fe1f94b75214ce07170d7d114f81ad
Adam Hardy <adh167@gmail.com>
2021-03-10 20:49:33 +0000

    change of mind on thread approach

--- multi.py
+++ multi.py
@@ -21,23 +21,18 @@ write_buffer_count = 1
 get_extended_workout_data = config["get_extended_workout_data"]
 get_profile_data = config["get_profile_data"]
 
-# workouts_file = "C2Rankings_" + strftime("%d%m%Y_%H%M", gmtime()) + ".json"
-# athletes_file = "C2Athletes_" + strftime("%d%m%Y_%H%M", gmtime()) + ".json"
-# extended_file = "C2Extended_" + strftime("%d%m%Y_%H%M", gmtime()) + ".json"
 workouts_file = config["workouts_file"]
 athletes_file = config["athletes_file"]
 extended_file = config["extended_file"]
 athletes_cache_file = config["athletes_cache_file"]
 extended_cache_file = config["extended_cache_file"]
 url_profile_base = config["url_profile_base"]
-urls_visited = 0
 
 athlete_profiles = {}
 workouts = {}
 ex_workout_data = {}
 
-
-urls = C2scrape.generate_C2Ranking_urls(config["url_query_parameters"], config["url_parameters"]["url_years"], config["url_parameters"]["url_events"], config["url_parameters"]["url_base"])
+urls_visited = 0
 
 if os.path.isfile(workouts_file):
     shutil.copyfile(workouts_file, workouts_file + "_backup")
@@ -68,15 +63,18 @@ else:
         athlete_profiles_cache = {}
         ex_workout_data_cache = {}
 
-num_ranking_urls = len(urls)
-if config["max_ranking_urls"] != "":
-    num_ranking_urls = int(config["max_ranking_urls"])
+ranking_tables = C2scrape.generate_C2Ranking_urls(config["url_query_parameters"], config["url_parameters"]["url_years"], config["url_parameters"]["url_events"], config["url_parameters"]["url_base"])
+num_ranking_tables = len(ranking_tables)
 
-ranking_url_count = 0 
-for url in urls[0:num_ranking_urls+1]: 
+if config["max_ranking_tables"] != "":
+    num_ranking_tables = int(config["max_ranking_tables"])
 
-    r = C2scrape.get_url(url.url_string)
-    ranking_url_count = ranking_url_count + 1
+num_ranking_tables = 0 
+
+for ranking_table in ranking_tables[0:num_ranking_tables+1]: 
+
+    r = C2scrape.get_url(ranking_table.url_string)
+    num_ranking_tables = num_ranking_tables + 1
     #find the number of pages for this ranking table
     if r != None:
         tree = html.fromstring(r.text)
@@ -90,22 +88,22 @@ for url in urls[0:num_ranking_urls+1]:
             pages = 1
     
     for page in range(1,pages+1):
-        #visit each page using the master thread, then give each row to a thread
-        #conceptually easier to implement at first then giving each page to a thread (though more overhead) as there should not be any duplicate workouts or profiles on a page and don't have to worry about adding duplicate entries to the datastructures but still give a big speed boost
-        url_string = url.url_string + "&page=" + str(page)
-        urls_visited = urls_visited + 1
+        #master thread loop over each page
+        url_string = ranking_table.url_string + "&page=" + str(page)
+
         
-        print(C2scrape.get_str_ranking_table_progress(urls_visited, ranking_url_count,num_ranking_urls,page,pages) + "Getting ranking page: " + url_string)
+        print(C2scrape.get_str_ranking_table_progress(urls_visited, num_ranking_tables,num_ranking_tables,page,pages) + "Getting ranking page: " + url_string)
         if page > 1:
             #don't get the first page again (if page is ommitted, page 1 is loaded)
             workouts_page=[]
             r = C2scrape.get_url(url_string)
-
+        
+        #master thread checks each row, adds URLs to queue for threads to visit
         if r != None:
             tree = html.fromstring(r.text)
             table_tree = tree.xpath('//html/body/div[2]/div/main/section[@class="content"]/table')
             
-            #get column headings
+            #get column headings for this page
             if table_tree != []:
                 columns = table_tree[0].xpath('thead/tr/th')
                 column_headings = [column.text for column in columns]
@@ -113,24 +111,42 @@ for url in urls[0:num_ranking_urls+1]:
                 for headings in column_headings:
                     row_dict = {}
 
-                num_rows = int(config["columns_per_page"])
+                #num_rows = int(config["columns_per_page"])
+                rows_tree = table_tree[0].xpath('tbody/tr')
+                num_rows = len(rows_tree)
 
-                #now we have the headers for the page, can mutlithread going through the rows
-                for row in range(1,num_rows+1):
-                    row_tree = table_tree[0].xpath('tbody/tr[' + str(row) + ']')
+                for row in range(0,num_rows):
+                    row_tree = rows_tree[row]
                     if row_tree != []:
-                        #TODO: refactor this so print strings inside loop don't need as many variables
-                        workout_data, ex_workout_data_profile, athlete_profile, profile_ID, workout_ID, urls_visited = C2scrape.get_workout_from_ranking_table_row(row_tree, url, column_headings, urls_visited, ranking_url_count, num_ranking_urls, page, pages, row, num_rows, get_extended_workout_data, get_profile_data, ex_workout_data_cache,athlete_profiles_cache, url_profile_base)                                
-                        #update global lists           
-                        if athlete_profile != None:
-                            athlete_profiles[profile_ID] = athlete_profile
-                            athlete_profiles_cache[profile_ID] = athlete_profile    
-                        if ex_workout_data_profile != None:
-                            ex_workout_data_cache[workout_ID] = ex_workout_data_profile
-                            ex_workout_data[workout_ID] = ex_workout_data_profile  
-                        if workout_data != []:
-                            workouts[workout_ID] = workout_data
-                    
+                        #get profile ID and workout ID
+                        workout_info_link_tree = row_tree.xpath('td/a')[0].attrib["href"]
+                        profile_ID = None
+                        workout_ID = None
+                        if workout_info_link_tree.split("/")[-2] == "individual" or workout_info_link_tree.split("/")[-2] == "race":
+                            profile_ID = workout_info_link_tree.split("/")[-1]
+                            workout_ID = workout_info_link_tree.split("/")[-3]
+                        else:
+                            workout_ID = workout_info_link_tree.split("/")[-2]
+                        #check both these in the cache
+                        if get_profile_data == True and profile_ID != None: 
+                            if profile_ID in athlete_profiles_cache.keys():
+                                #TODO: retrieve data from cache
+                            else:
+                                #add profile url to queue
+                        
+                        
+                        #if don't exist, add to thread queue
+                        #add row workout data to workouts list
+         
+                # if athlete_profile != None:
+                #     athlete_profiles[profile_ID] = athlete_profile
+                #     athlete_profiles_cache[profile_ID] = athlete_profile    
+                # if ex_workout_data_profile != None:
+                #     ex_workout_data_cache[workout_ID] = ex_workout_data_profile
+                #     ex_workout_data[workout_ID] = ex_workout_data_profile  
+                # if workout_data != []:
+                #     workouts[workout_ID] = workout_data
+            
 
             #at the end of a page, check to see if we should write to file
             if write_buffer_count % write_buffer == 0:
