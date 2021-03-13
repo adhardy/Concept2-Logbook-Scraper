e3e52336c6f78f78957e32e44503b770bd6f0877
Adam Hardy <adh167@gmail.com>
2021-03-13 11:43:31 +0000

    updated with analysis files from main branch

--- analyse.py
+++ analyse.py
@@ -1,26 +1,41 @@
-import json
+# %%
 
+import json
+import pandas as pd
+# %%
 try:
-    fo = open("C2Athletes.json")
-    athletes = json.load(fo)
-    fo.close
+    #fo = open("output/C2Athletes_cache.json")
+    #athletes = json.load(fo)
+    df_athletes = pd.read_json("output/C2Athletes_cache.json", orient='index')
+    #fo.close
 except:
     print("Could not load athletes data file.")
     athletes={}
 
 try:
-    fo = open("C2Rankings.json")
-    rankings = json.load(fo)
-    fo.close
+    # fo = open("output/C2Rankings.json")
+    # rankings = json.load(fo)
+    # fo.close
 except:
     rankings={"Could not load workouts data file."}
 
+try:
+    # fo = open("output/C2Extended_cache.json")
+    # rankings = json.load(fo)
+    # fo.close
+except:
+    rankings={"Could not load workouts data file."}
+
+# %%
+print(df_athletes)
+# %%
 #get basic stats:
 
-#number of workouts
+#number of workout
+print("Number of workouts: {}".format(len(rankings)))
 #number of workouts of each event
 
 #number of public athlete profiles
+
 #number of private athlete profiles
 #number of logged in only athlete profiles
-
