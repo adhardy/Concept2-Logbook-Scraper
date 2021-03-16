d66d9327abb6a796bf79fb13cfd0c28b99cb2182
Adam Hardy <adh167@gmail.com>
2021-03-16 13:46:17 +0000

    updated example job function

--- multi_webbing.py
+++ multi_webbing.py
@@ -103,7 +103,16 @@ class Job:
 
 def job_function_template(job):
     """not used, template code for a job_function"""
-    job_data = {}
+
+    #import multi_webbing as mw
+    #num_threads = 4
+    #my_threads = mw.MultiWebbing(num_threads) #intialize threading
+    #my_threads.start #start threads, they are now checking the queue, waiting for work
+    #my_threads.job_queue.put(mw.Job(job_id, my_job_function, url, [job_data, job_type])) #put a job in the queue
+
+    job_data = job.custom_data[0] #in this example, a dictionary which contains the data being scraped
+    job_type = job.custom_data[1] #in this example, a string
+
     job.get_url() #get the URL
     if job.request != None: #check that a URL was recieved OK, will be None if there as a problem
         job.lock.acquire() #update/append are thread safe but other operations elsewhere (e.g. JSON.dumps) might not be
