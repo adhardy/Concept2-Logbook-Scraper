4163342f058dc9c639e0fb9b5f6de6d0e031a6fb
Adam Hardy <adh167@gmail.com>
2021-03-16 13:13:10 +0000

    moved Thread class with MultiWebbing class

--- multi_webbing.py
+++ multi_webbing.py
@@ -11,7 +11,7 @@ class MultiWebbing():
         self.session = requests.session()
         self.threads = []
         for i in range(num_threads):
-            self.threads.append(Thread(i, self.job_queue, self.lock, self.session))
+            self.threads.append(self.Thread(i, self.job_queue, self.lock, self.session))
 
     def start(self):
         """Call after initiating a Threading object to start the threads."""
@@ -23,46 +23,45 @@ class MultiWebbing():
         for thread in self.threads:
             thread.join()
 
-class Thread(threading.Thread):
-    #define how the threads function
-    #TODO add verbosity for more detailed output options
-    def __init__(self, number, job_queue, lock, session):
-        threading.Thread.__init__(self)
-        self.number = number
-        self._stop_event = threading.Event()
-        self.job_queue = job_queue
-        self.lock = lock
-        self.session = session
+    class Thread(threading.Thread):
+        #define how the threads function
+        #TODO add verbosity for more detailed output options
+        def __init__(self, number, job_queue, lock, session):
+            threading.Thread.__init__(self)
+            self.number = number
+            self._stop_event = threading.Event()
+            self.job_queue = job_queue
+            self.lock = lock
+            self.session = session
 
-    def run(self):
-        #execute on thread.start()
-        #job_function should have a Job object as its sole argument. Can update job argument with additional attributes if needed for the function
-        print(f" ** Starting thread - {self.number}")
+        def run(self):
+            #execute on thread.start()
+            #job_function should have a Job object as its sole argument. Can update job argument with additional attributes if needed for the function
+            print(f" ** Starting thread - {self.number}")
 
-        while not self._stop_event.isSet():
-            #thread will continuously check the queue for work until the master process joins the threads, and the stop_event signal is sent
-            try:
-                #get a job
-                job = self.job_queue.get(block=False)
-                job.set_thread(self) #give job access to thread attributes
+            while not self._stop_event.isSet():
+                #thread will continuously check the queue for work until the master process joins the threads, and the stop_event signal is sent
+                try:
+                    #get a job
+                    job = self.job_queue.get(block=False)
+                    job.set_thread(self) #give job access to thread attributes
 
-            except queue.Empty:
-                pass
+                except queue.Empty:
+                    pass
 
-            else:
-                #print("Thread " + self.name + ": Getting profile: " + profile.url)
-                #execute main thread function
-                job.function(job)
-                #update the data structure with the returned data
+                else:
+                    #print("Thread " + self.name + ": Getting profile: " + profile.url)
+                    #execute main thread function
+                    job.function(job)
+                    #update the data structure with the returned data
 
+            print(f" ** Completed thread - {self.number}")
 
-        print(f" ** Completed thread - {self.number}")
-
-    def join(self, timeout=None):
-        #send stop event to terminate the work loop before calling join
-        self._stop_event.set()
-        super().join(timeout)
-        print(f" ** Joined thread - {self.number}")
+        def join(self, timeout=None):
+            #send stop event to terminate the work loop before calling join
+            self._stop_event.set()
+            super().join(timeout)
+            print(f" ** Joined thread - {self.number}")
 
 class Job:
     #holds all the information needed for the worker threads to make a request and execute the job_function
