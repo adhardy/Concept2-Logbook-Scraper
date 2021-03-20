75a253d01f07611a921440a9ec7d3c67c7ffc814
Adam Hardy <adh167@gmail.com>
2021-03-20 19:47:07 +0000

    cleaned up columns

--- C2Analyse.py
+++ C2Analyse.py
@@ -1,20 +1,23 @@
 import json
 import pandas as pd
+import numpy as np
 
-class C2Analyse():
+class df():
 
     def __init__(self):
         self.event_map = {"1":"1 minute", "4":"4 minute", "30":"30 minute", "60":"60 minute"}
 
-    def load_C2scrape_data(path):
+    def load_from_file(path):
         try:
             fa = open(path, "r")
             df = pd.DataFrame.from_dict(json.load(fa)).T
+            df.replace({"":np.nan, "None":np.nan, "None":None}, inplace=True) #all missing values to nan
         except:
             print("Could not load workouts data file.")
             df = None
+            
         return df
 
-    def merge_dataframes(df_workouts, df_athletes, df_extended, how="inner"):
-        pd.merge(left=(pd.merge(left=df_workouts, right=df_athletes, left_on='profile_id', right_on="profile_id", right_index=True, how=how)), 
+    def merge(df_workouts, df_athletes, df_extended, how="inner"):
+        return pd.merge(left=(pd.merge(left=df_workouts, right=df_athletes, left_on='profile_id', right_on="profile_id", right_index=True, how=how)), 
             right = df_extended, left_on='workout_id', right_on="workout_id", right_index=True, how=how)
