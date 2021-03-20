6aa52aa8b38d7a211b6e799853d521146f437091
Adam Hardy <adh167@gmail.com>
2021-03-20 17:05:51 +0000

    Started adding markdown to analysis notebook

--- C2Analyse.py
+++ C2Analyse.py
@@ -1,11 +1,17 @@
 import json
 import pandas as pd
 
-def load_C2scrape_data(path):
-    try:
-        fa = open(path, "r")
-        df = pd.DataFrame.from_dict(json.load(fa)).T
-    except:
-        print("Could not load workouts data file.")
-        df = None
-    return df
+class C2Analysis():
+
+    def __init__(self)
+        self.event_map = {"1":"1 minute", "4":"4 minute", "30":"30 minute", "60":"60 minute"}
+
+    def load_C2scrape_data(path):
+        try:
+            fa = open(path, "r")
+            df = pd.DataFrame.from_dict(json.load(fa)).T
+        except:
+            print("Could not load workouts data file.")
+            df = None
+        return df
+
